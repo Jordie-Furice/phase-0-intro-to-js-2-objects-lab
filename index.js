6fde89a03c9409bbf992637f4a0117139605c177
Jordie-Furice <furice@gmail.com>
2026-07-04 12:46:34 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+var employee = {
+	name: 'Sam',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+	return {
+		...employee,
+		[key]: value,
+	};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+	employee[key] = value;
+	return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+	var newEmployee = {
+		...employee,
+	};
+
+	delete newEmployee[key];
+	return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+	delete employee[key];
+	return employee;
+}
